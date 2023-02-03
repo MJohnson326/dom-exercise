4117fa7577342beddf20485c028c704d0ef0902a
Melissa Johnson <melissajohnson326@gmail.com>
2023-02-02 22:06:40 -0500

    add h3 and style to blue in DOM

--- js-file.js
+++ js-file.js
@@ -7,3 +7,13 @@ para.textContent = 'Hey I\'m red!'; //add text content to p
 container.appendChild(para);// add to DOM and div
 
 para.style.color = 'red'// change text color
+
+
+
+const blue = document.createElement('h3');//create h3
+blue.classList.add('blue');// add class to h3
+blue.textContent = 'I\'m a blue h3! '; //add text content to p
+
+container.appendChild(blue);// add to DOM and div
+
+blue.style.color = 'blue'// change text color
