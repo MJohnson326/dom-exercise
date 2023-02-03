339226e29368841a271af323a7a29e4704d9071e
Melissa Johnson <melissajohnson326@gmail.com>
2023-02-02 22:43:56 -0500

    add h1 and p inside container 2

--- js-file.js
+++ js-file.js
@@ -17,8 +17,21 @@ container.appendChild(blue);// add to DOM and div
 
 
 
-const container2 = document.createElement('div');
-container2.classList.add('container2');
-container2.setAttribute('style', 'border:black solid 10px; background: pink;');  
-container2.textContent='akjhd;fkgahd;ghd'
-container.appendChild(container2);
+const container2 = document.createElement('div');//create new div
+container2.classList.add('container2');//add class 'container2' to div
+container2.setAttribute('style', 'border:black solid 10px; background: pink;');  //add bg color and border
+
+const header1 = document.createElement('h1');//create h1 
+header1.classList.add('header1')// add class header1 to h1
+header1.textContent = 'I\'m in a div!'// add text to h1
+container2.appendChild(header1);// add h1 to div.container2
+
+const para2 = document.createElement('p');//create p element
+para2.classList.add('para2');// add class para2 to p element
+para2.textContent='ME TOO!';//add text to para2
+container2.appendChild(para2)// add para2 to container2
+
+
+
+
+container.appendChild(container2); //add all div.container2 to .container
