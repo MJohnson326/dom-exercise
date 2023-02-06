ce6b8045c695bceade03642cfe3965a66a56753e
Melissa Johnson <melissajohnson326@gmail.com>
2023-02-06 18:19:21 -0500

    add div with button and id and add even listener 'click' and function alert id

--- js-file.js
+++ js-file.js
@@ -38,7 +38,7 @@ container.appendChild(container2); //add all div.container2 to .container
 
 
 
-
+/*
 const btn = document.querySelector('#btn');
 btn.addEventListener('click', () => {
   alert("Hello World");
@@ -53,4 +53,13 @@ function alertFunction() {
 
   btn.addEventListener('click', function (e) {
     e.target.style.background = 'blue';
+  });
+
+  */
+  const buttons= document.querySelectorAll('button');
+
+  buttons.forEach((button)=> {
+button.addEventListener('click', ()=> {
+  alert(button.id);
+});
   });
